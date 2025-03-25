440669fe12b2e2655e823f2868b2ace8bdb40a66
Dirk Van Haerenborgh <dirk.vanhaerenborgh@aloxy.io>
2025-03-25 20:53:13 +0100

    allow gnome shell extensions by name

--- modules/home-manager/gnome/gnome-shell-extensions.nix
+++ modules/home-manager/gnome/gnome-shell-extensions.nix
@@ -10,9 +10,17 @@ with lib; let
   extensionConfigType = types.submodule {
     options = {
       pkg = mkOption {
-        type = types.package;
+        type = types.nullOr types.package;
+        default = null;
         description = "The extension package";
         example = "pkgs.gnomeExtensions.dash-to-dock";
+      };
+
+      uuid = mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        description = "The uuid of the extension";
+        example = "GPaste@gnome-shell-extensions.gnome.org";
       };
 
       enable = mkOption {
@@ -41,35 +49,63 @@ with lib; let
     };
   };
 
-  hasExactFields = attrs:
-    builtins.length (builtins.attrNames attrs)
-    >= 3
-    && builtins.hasAttr "pkg" attrs
-    && builtins.hasAttr "enable" attrs
-    && builtins.hasAttr "settings" attrs;
+  # Parse a single extension configuration entry
+  parseExtensionConfig = ext: let
+    # Determine the package
+    pkg =
+      if lib.isString ext then null
+      else if (builtins.hasAttr "pkg" ext) && (ext.pkg != null) then ext.pkg
+      else if ext != null
+      then ext
+      else null;
 
-  extensionList =
-    map (
-      ext:
-        if hasExactFields ext
-        then ext
-        else {
-          pkg = ext;
-          enable = true;
-          settingsKey = ext.extensionPortalSlug;
-          settings = {};
-        }
-    )
-    cfg.extensions;
+    # Determine the uuid
+    uuid =
+      if lib.isString ext then ext
+      else if (builtins.hasAttr "uuid" ext) && (ext.uuid != null)
+      then ext.uuid
+      else if pkg != null
+      then pkg.extensionUuid
+      else if ext != null
+      then ext
+      else null;
+
+    # Determine enable status
+    enable =
+      if lib.isString ext
+      then true
+      else if ext ? enable
+      then ext.enable
+      else true;
+
+    # Determine settings key
+    settingsKey =
+      if lib.isString ext then uuid
+      else if (builtins.hasAttr "settingsKey" ext) && (ext.settingsKey != "")
+      then ext.settingsKey
+      else uuid;
+
+    # Determine settings
+    settings =
+      if lib.isString ext
+      then {}
+      else if ext ? settings
+      then ext.settings
+      else {};
+  in
+    # Validate that either pkg or uuid is present
+    if (pkg == null) && (uuid == null)
+    then throw "Extension configuration must have either a package or a uuid"
+    else {
+      inherit pkg uuid enable settingsKey settings;
+    };
+
+  extensionList = map parseExtensionConfig cfg.extensions;
 
   # Generate dconf settings for an extension
-  mkDConfSettings = extension: let
-    settingsPath = "org/gnome/shell/extensions/${
-      if extension.settingsKey == ""
-      then extension.pkg.extensionPortalSlug
-      else extension.settingsKey
-    }";
-    nestedAttrs = builtins.all (value: builtins.isAttrs value) (builtins.attrValues extension.settings);
+  mkDConfSettings = ext: let
+    settingsPath = "org/gnome/shell/extensions/${ext.settingsKey}";
+    nestedAttrs = builtins.all (value: builtins.isAttrs value) (builtins.attrValues ext.settings);
   in
     if nestedAttrs
     then
@@ -78,16 +114,16 @@ with lib; let
         subPath: subSettings:
           nameValuePair "${settingsPath}${subPath}" subSettings
       )
-      extension.settings
+      ext.settings
     else {
-      ${settingsPath} = extension.settings;
+      ${settingsPath} = ext.settings;
     };
 in {
   options.custom.programs.gnome-shell-extensions = {
     enable = mkEnableOption "GNOME Shell extensions management";
 
     extensions = mkOption {
-      type = types.listOf (types.either types.package extensionConfigType);
+      type = types.listOf (types.oneOf [types.package types.str extensionConfigType]);
       default = [];
       description = "List of GNOME Shell extensions to install and configure";
       example = literalExpression ''
@@ -101,6 +137,7 @@ in {
               "extend-height" = true;
             };
           }
+          "extension@gnome.org"
         ]
       '';
     };
@@ -108,12 +145,12 @@ in {
 
   config = mkIf cfg.enable {
     # Ensure GNOME Shell is installed and install extensions
-    home.packages = [pkgs.gnome-shell] ++ map (e: e.pkg) (filter (e: e.enable) extensionList);
+    home.packages = [pkgs.gnome-shell] ++ map (e: e.pkg) (filter (e: e.enable && e.pkg != null) extensionList);
 
     # Install and configure extensions
     dconf.settings = let
       # Fill out dconf settings
-      extensionSettings = filter (set: attrNames set != []) (map (ext: mkDConfSettings ext) extensionList);
+      extensionSettings = filter (set: attrNames set != []) (map mkDConfSettings extensionList);
     in
       mkMerge (
         # Enable/disable extensions
@@ -121,8 +158,8 @@ in {
           {
             "org/gnome/shell" = {
               disable-user-extensions = false; # enables user extensions
-              enabled-extensions = map (e: e.pkg.extensionUuid) (filter (e: e.enable) extensionList);
-              disabled-extensions = map (e: e.pkg.extensionUuid) (filter (e: !e.enable) extensionList);
+              enabled-extensions = map (e: builtins.toString e.uuid) (filter (e: e.enable) extensionList);
+              disabled-extensions = map (e: builtins.toString e.uuid) (filter (e: !e.enable) extensionList);
             };
           }
         ]
